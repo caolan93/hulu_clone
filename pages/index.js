a80ee747d9ce3c531c06d2f48d3fe0a493095136
Caolan Fanning <caolan.fanning@gmail.com>
2021-08-06 14:25:16 +0100

    enviornment variable

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
 
 export default function Home() {
 	return (
@@ -13,6 +15,11 @@ export default function Home() {
 			</Head>
 			<Header />
 			<Nav />
+			<Results />
 		</div>
 	);
+}
+
+export async function getServerSideProps(context) {
+	const genre = context.query.genre;
 }
